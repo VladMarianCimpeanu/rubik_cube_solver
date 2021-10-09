21b6c41dfd6761d4a3054b227e6794ee2495e895
Vlad Marian Cimpeanu <62434812+VladMarianCimpeanu@users.noreply.github.com>
2021-10-09 19:08:15 +0200

    implemented rubik model

--- venv/rubik_cube/face.py
+++ venv/rubik_cube/face.py
@@ -1,12 +1,18 @@
 from colors import Color
 
 class Face:
-    def __init__(self, color: Color):
+    def __init__(self, *args):
         self.face = []
-        for row in range(3):
-            self.face.append([])
-            for column in range(3):
-                self.face[row].append(color.name)
+        """if args length is 1, the argument must be a Color object and the face is initialized to a face with the selected color """
+        """if args lenght is 2, the arguments must be 3 lists of 3 Colors. """
+        if len(args) == 1:
+            for row in range(3):
+                self.face.append([])
+                for column in range(3):
+                    self.face[row].append(args[0].name)
+        else:
+            for item in args[0]:
+                self.face.append(item)
 
     def get_row(self, row: int) -> list:
         return self.face[row].copy()
@@ -26,15 +32,16 @@ class Face:
             row[column_index] = blocks[column_index]
         return old_column
 
+    def get_face(self):
+        return Face([row.copy() for row in self.face], True)
+
     def print_face(self) -> None:
         for row in self.face:
             print(row)
 
 if __name__ == "__main__":
     face = Face(Color.RED)
+    copied_face = face.get_face()
+    copied_face.set_row(0, [Color.BLUE.name for item in range(3)])
     face.print_face()
-    temp_row = face.get_row(0)
-    face.set_row(0, [Color.BLUE.name for item in range(3)])
-    print(temp_row)
-    print("\n")
-    face.print_face()
+    copied_face.print_face()
--- venv/rubik_cube/rubik_cube.py
+++ venv/rubik_cube/rubik_cube.py
@@ -1,13 +1,61 @@
 from colors import Color
 from direction import Direction
+from face import Face
 
 class Cube:
-    horizontal_direction = [1, 3, 2]
-    vertical_direction = [4, 3, 5]
-    def __init__(self):
 
+    def __init__(self, *args):
+        self.horizontal_direction = [1, 3, 2, 0]
+        self.vertical_direction = [4, 3, 5, 0]
+        self.faces = []
+        if len(args) == 0:
+            for color in (Color):
+                self.faces.append(Face(color))
+        else:
+            for face in args[0]:
+                self.faces.append(face)
 
+    def show_cube(self):
+        for face in self.faces:
+            face.print_face()
+
+    def switch_columns(self, direction: list(), position: int) -> None:
+        last_visited_face = 0
+        last_visited_column = (self.faces[last_visited_face]).get_column(position)
+        for face_to_visit in direction:
+            last_visited_column = (self.faces[face_to_visit]).set_column(position, last_visited_column)
+            last_visited_face = face_to_visit
+
+    def switch_rows(self, direction: list(), position: int) -> None:
+        last_visited_face = 0
+        last_visited_row = (self.faces[last_visited_face]).get_row(position)
+        for face_to_visit in direction:
+            last_visited_row = (self.faces[face_to_visit]).set_row(position, last_visited_row)
+            last_visited_face = face_to_visit
+
+    def rotate(self, direction: Direction, position: int) -> None:
+        if direction == direction.UP:
+            self.switch_columns(self.vertical_direction, position)
+        elif direction == direction.DOWN:
+            self.switch_columns(self.vertical_direction.reverse(), position)
+        elif direction == direction.RIGHT:
+            self.switch_rows(self.horizontal_direction, position)
+        else:
+            self.switch_rows(self.horizontal_direction.reverse(), position)
+
+    def get_face(self, face: int):
+        return self.faces[face].get_face()
+
+    def get_cube(self) -> list:
+        return [face.get_face() for face in self.faces]
 
 
 if __name__ == "__main__":
     cube = Cube()
+    other_cube = Cube(cube.get_cube())
+    other_cube.rotate(Direction.UP, 2)
+    cube.show_cube()
+    print("\n\n")
+    other_cube.show_cube()
+
+
