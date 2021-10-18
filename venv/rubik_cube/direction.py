1171e37a85da6c737357c0e0179144e4d6238e36
Vlad Marian Cimpeanu <62434812+VladMarianCimpeanu@users.noreply.github.com>
2021-10-18 20:32:29 +0200

    added the possibility to check if 2 directions are opposite or not in Direction class

--- venv/rubik_cube/direction.py
+++ venv/rubik_cube/direction.py
@@ -6,4 +6,11 @@ class Direction(enum.Enum):
     RIGHT = 2
     LEFT = -2
 
+    def is_opposite(self, direction):
+        return self.value == -direction.value
 
+if __name__ == "__main__":
+    """should be TRUE"""
+    print(Direction.UP.is_opposite(Direction.DOWN))
+    """should be FALSE"""
+    print(Direction.UP.is_opposite(Direction.RIGHT))
--- venv/rubik_cube/node.py
+++ venv/rubik_cube/node.py
@@ -14,7 +14,7 @@ class Node:
         for dir in Direction:
             for pos in range(3):
                 """it is avoiding to generate a child containing the same cube of its parent node."""
-                if not ((self.obtained_from is None or dir.value == -self.obtained_from.value)
+                if not ((self.obtained_from is None or dir.is_opposite(self.obtained_from))
                         and pos == self.positon_from):
                     new_cube = self.cube.copy_cube()
                     new_cube.rotate(dir, pos)
